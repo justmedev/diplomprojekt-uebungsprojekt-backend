289ed5d740218df7e8478ce389ec04a0d731eb9d
Thomas <166368507+Thomas02024@users.noreply.github.com>
2026-08-25 17:58:59 +0200

    refactor: /api/pdf -> /api/pdfs

--- backend/src/main/java/at/zettasecure/diplomprojekt/pdfeditor/feature/pdfmanager/uc/delete/PdfEndpoint.java
+++ backend/src/main/java/at/zettasecure/diplomprojekt/pdfeditor/feature/pdfmanager/uc/delete/PdfEndpoint.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/pdf")
+@RequestMapping("/api/pdfs")
 @AllArgsConstructor
 public class PdfEndpoint {
 
